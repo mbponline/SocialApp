d8d562dfaeef69c72e8c0f1f9b0ae60dc867feaa
Nikolaj Månsson <nikolovescode>
2019-10-31 14:24:02 +0100

    Allow cors and first collect of api value

--- restaurant-client/src/App.tsx
+++ restaurant-client/src/App.tsx
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-const App: React.FC = () => {
+class App extends Component{
+  //Initiates values
+  state = {
+    values: []
+  }
+//First method called
+  componentDidMount(){
+    axios.get('http://localhost:5000/api/values')
+    .then((response) => {
+      console.log(response);
+          //updates state and causes a render which gets data from our api
+      this.setState({
+        values: response.data
+      })
+    })
+  }
+  render(){
   return (
     <div className="App">
-     
+     <header className='App-header'>
+      <img src={logo} className='App-logo' />
+      <ul>
+        {this.state.values.map((value: any) => (
+          <li>{value.name}</li>
+        ))}
+      </ul>
+     </header>
     </div>
-  );
+  ); 
+}
 }
 
 export default App;
