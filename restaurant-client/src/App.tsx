e5b688293349c4c8e9f0b46287c9d28193514512
Nikolaj Månsson <nikolovescode>
2019-10-31 14:37:10 +0100

    Client: Added SemanticUI and implemented for header and list

--- restaurant-client/src/App.tsx
+++ restaurant-client/src/App.tsx
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import { Header, Icon, List } from 'semantic-ui-react';
+
 
 class App extends Component{
   //Initiates values
@@ -12,7 +13,6 @@ class App extends Component{
   componentDidMount(){
     axios.get('http://localhost:5000/api/values')
     .then((response) => {
-      console.log(response);
           //updates state and causes a render which gets data from our api
       this.setState({
         values: response.data
@@ -21,15 +21,18 @@ class App extends Component{
   }
   render(){
   return (
-    <div className="App">
-     <header className='App-header'>
-      <img src={logo} className='App-logo' />
-      <ul>
-        {this.state.values.map((value: any) => (
-          <li>{value.name}</li>
+    <div>
+        <Header as='h2' icon>
+    <Icon name='users' />
+    <Header.Content>
+      Restaurants
+    </Header.Content>
+  </Header>
+  <List>
+  {this.state.values.map((value: any) => (
+          <List.Item key={value.id}>{value.name}</List.Item>
         ))}
-      </ul>
-     </header>
+  </List>
     </div>
   ); 
 }
